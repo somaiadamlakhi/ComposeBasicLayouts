db08d9b3fcee3402229983928db2292a5420c294
Somaia Damlakhi <somia.damlakhi@1001.tv>
2026-07-31 14:57:51 -0400

    add state hosting example with counter as an example of Unidirectional Data Flow

--- app/src/main/java/com/blueray/rowsandcolumns/state_management/StateHosting.kt
+++ app/src/main/java/com/blueray/rowsandcolumns/state_management/StateHosting.kt
@@ -1,46 +1,49 @@
 package com.blueray.rowsandcolumns.state_management
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
-
 @Composable
-fun Counter(
-    modifier: Modifier = Modifier,
-    counter: Int,
-    onCounterButtonClick: () -> Unit
-) {
+fun StateHostingDemo(modifier: Modifier = Modifier) {
 
-    Box(
-        contentAlignment = Alignment.Center
+    var counter by rememberSaveable {
+        mutableIntStateOf(0)
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        Counter(
+            counter = counter,
+            onCounterButtonClick = {
+                counter++
+            }
+        )
         Button(
             onClick = {
-                onCounterButtonClick()
+                counter = 0
             }
         ) {
-            Text("Count $counter")
+            Text("Reset Counter")
         }
-
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-private fun CounterPrev() {
-    Counter(
-        counter = 0,
-        onCounterButtonClick = {}
-    )
+private fun StateHostingPrev() {
+    StateHostingDemo()
 }
